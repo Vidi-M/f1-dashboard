09cd04f221365e4abbedcf0f07198acf6854042f
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2025-01-23 01:30:18 +0000

    added buttons to go between the two pages

--- f1/dashboard/views.py
+++ f1/dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def dashboard(request):
-    return HttpResponse("You have reached the dashboard")
+    template = loader.get_template('dashboard.html')
+    return HttpResponse(template.render())
--- f1/home_page/views.py
+++ f1/home_page/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def home(request):
-    return HttpResponse("Welcome!!! This is the home page")
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
